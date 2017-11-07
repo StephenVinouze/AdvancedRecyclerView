3079638f8bc6df135838c2b858464617a7cbcba1
StephenVinouze <s.vinouze@gmail.com>
2017-11-07 17:29:06 +0100

    Fix package names for different modules

--- core/src/main/kotlin/com/github/stephenvinouze/advancedrecyclerview_core/views/BaseViewHolder.kt
+++
@@ -1,9 +0,0 @@
-package com.github.stephenvinouze.advancedrecyclerview_core.views
-
-import android.support.v7.widget.RecyclerView
-import android.view.View
-
-/**
- * Created by Stephen Vinouze on 04/11/2015.
- */
-open class BaseViewHolder(val view: View) : RecyclerView.ViewHolder(view)
